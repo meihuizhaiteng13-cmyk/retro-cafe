240d525fbae5de24d95dc59e44df877f84e2da5a
meihuizhaiteng13-cmyk <meihuizhaiteng13@gmail.com>
2026-01-23 21:58:04 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+/* ===== スライドショー ===== */
+const slides = document.querySelectorAll('.slide');
+let currentIndex = 0;
+
+setInterval(() => {
+  slides[currentIndex].classList.remove('active');
+  currentIndex = (currentIndex + 1) % slides.length;
+  slides[currentIndex].classList.add('active');
+}, 5000);
